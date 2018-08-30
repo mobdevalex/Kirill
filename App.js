ac1c2743bb5c6ee63200f079eb3bb11db5aa8b51
mobdevalex <for.magnit@hotmail.com>
2018-08-30 23:18:02 +0100

    vote in process

--- App.js
+++ App.js
@@ -28,7 +28,6 @@ export default class App extends Component<{}> {
                             <Text style={styles.TextStyle}>{item.id}) {item.preview} {item.text} </Text>
 
 
-
                             {/*vote*/}
                             <View style={styles.voteView}>
 
